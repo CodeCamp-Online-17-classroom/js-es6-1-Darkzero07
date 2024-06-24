59f77bc55d251a0f0e2699dc508aad3e74dd75d8
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-24 12:39:23 +0700

    js lab 145

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// write code here
+// จงเขียนฟังก์ชันหาค่าผลคูณของตัวเลข โดยพารามิเตอร์ของฟังก์ชันสามารถรับตัวเลขได้ไม่จำกัด
+
+function multiplyNumbers(...numbers) {
+  if (numbers.length === 0) {
+    return 0;
+  } else {
+    return numbers.reduce((acc, num) => acc * num);
+  }
+}
+
+console.log(multiplyNumbers(2, 3, 4)); // 24 (2 * 3 * 4 = 24)
+console.log(multiplyNumbers(5, 6)); // 30 (5 * 6 = 30)
+console.log(multiplyNumbers(10)); // 10
+console.log(multiplyNumbers()); //0
